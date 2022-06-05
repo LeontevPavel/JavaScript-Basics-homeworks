e96c376815b0f53c578e2b78e7ffc5e77c8f1a3f
LeontevPavel <leontev@mail.ru>
2022-06-05 23:03:42 +0500

    first commit

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let discriminant;
+  discriminant = (b ** 2) - (4 * a * c);
+  if (discriminant < 0) {
+    return arr;
+  }	if (discriminant === 0) {
+		arr[0] = -b/(2*a);
+		return arr;		
+		}	else {
+		arr[0] = (-b + Math.sqrt(discriminant) )/(2*a);
+		arr[1] = (-b - Math.sqrt(discriminant) )/(2*a);
+		return arr;
+		}
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
