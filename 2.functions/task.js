a22429cc7ff7831fabe9040c848a205b38697ffc
LeontevPavel <leontev@mail.ru>
2022-06-18 00:42:54 +0500

    add function and arrays

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,58 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
+  let min = arr[0];
+  let max = arr[0];
+  let sum = 0;
+  let avg;
 
-  // Ваш код
+  for (let i = 0; i < arr.length; i++) {
+    let itemArr = arr[i];
+    if (itemArr < min) min = itemArr;
+    if (itemArr > max) max = itemArr;
+    sum = sum + itemArr;
+  }
+
+  avg = sum / arr.length;
+  avg = +avg.toFixed(2);
 
   return { min: min, max: max, avg: avg };
 }
 
+//getArrayParams(arr);
+
 // Задание 2
 function worker(arr) {
-  let sum;
-
-  // Ваш код
-
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += Number(arr[i]);
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
-  let max;
+  let max = -Infinity;
 
-  // Ваш кода
-  // for ...
-  
+  for (let i = 0; i < arrOfArr.length; i++) {
+    let arr = arrOfArr[i];
+    let sum = func(arr);
+    if (sum > max) max = sum;
+  }
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let difference = 0;
+  let max = -Infinity;
+  let min = Infinity;
+
+  for (let i = 0; i < arr.length; i++) {
+    let itemArr = Number(arr[i]);
+    if (itemArr < min) min = itemArr;
+    if (itemArr > max) max = itemArr;
+  }
+  difference = Math.abs(min - max);
+
+  return difference;
 }
+
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,31 @@
+
 function compareArrays(arr1, arr2) {
   let result;
+  if (arr1.length != arr2.length) return result = false;
 
-  // Ваш код
+  function compareItem(item, i) {
+    return item === arr2[i];
+  }
 
-  return result; // boolean
+  return result = arr1.every(compareItem);
+}
+
+console.log(compareArrays([1, 45], [1, 4]));
+
+
+function multipleThree(item) {
+	return item % 3 === 0 && item !=0;
+}
+
+function positive(item) {
+	return item > 0;	
+}
+
+function multiplication(item) {
+	return item * 10;
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
-}
+  let resultArr = arr.filter(multipleThree).filter(positive).map(multiplication);
+	return resultArr;
+}			
